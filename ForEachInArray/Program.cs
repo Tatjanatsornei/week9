dbd54831a149bb95f8ac3f5e7de0c907a66c0892
Tatjana Tsornei <tanjatsornei@hotmail.com>
2022-05-19 21:13:15 +0300

    add my nines c# project

---
+++ ForEachInArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForEachInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apples", "bananas", "oranges", "pears", "plums" };
+
+            foreach (string item in fruits)   //" для каждого элемента в массиве fruits
+            {
+                Console.WriteLine(item);
+                if (item == "oranges")
+                {
+                    Console.WriteLine($"Boo, I don't like {item}");
+                    // item = "tangerines"; - не сработает
+                }
+            }     
+        }
+    }
+}
